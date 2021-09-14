d7614ebf50c5b3433922fed3c44735e2590a17f3
xCrypt0r <legacy5877@gmail.com>
2021-09-15 02:56:53 +0900

    Remove unnecessary comments

--- Vivace/ListForm.Designer.cs
+++ Vivace/ListForm.Designer.cs
@@ -3,15 +3,8 @@ namespace Vivace
 {
     partial class ListForm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -21,12 +14,6 @@ namespace Vivace
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.lstMatched = new System.Windows.Forms.ListBox();
@@ -127,10 +114,7 @@ namespace Vivace
             this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
-
-        #endregion
 
         private System.Windows.Forms.ListBox lstMatched;
         private System.Windows.Forms.Label label1;
--- Vivace/Program.cs
+++ Vivace/Program.cs
@@ -5,9 +5,6 @@ namespace Vivace
 {
     static class Program
     {
-        /// <summary>
-        /// 해당 애플리케이션의 주 진입점입니다.
-        /// </summary>
         [STAThread]
         static void Main()
         {
